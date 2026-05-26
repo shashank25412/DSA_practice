445406e3a375187c9a35c6ec2cba390b76e61fc3
Shashank Kumar <74102663+shashank25412@users.noreply.github.com>
2026-05-26 11:20:01 +0530

    trapping rain water solution

--- 9. Two pointers/TrappingRainWater.cpp
+++ 9. Two pointers/TrappingRainWater.cpp
@@ -19,12 +19,33 @@ using namespace std;
 
     Intuition
     --------------------
-    Aproach1:
+    Aproach1:  Take 2 pointers, 1 from left another from right, at whichever point we get less height, we start moving
+    inwards from there, until we found something bigger than that, otherwise we keep adding the current pointers height
+    minus the height of the bar found in the way. We repeat this till left pointer crosses the right one.
 
 */
 
 int rainWaterStored(vector<int>& height){
     int res = 0;
+    int left = 0, right = height.size()-1;
+
+    while(left < right){
+        int minn = min(height[left], height[right]);
+        if(minn == height[left]){
+            left++;
+            while(left<right && height[left]<minn){
+                res += minn - height[left];
+                left++;
+            }
+        }
+        else{
+            right--;
+            while(left<right && height[right]<minn){
+                res += minn - height[right];
+                right--;
+            }
+        }
+    }
 
     return res;
 }
